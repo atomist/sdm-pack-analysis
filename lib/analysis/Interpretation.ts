96af1e1d332fcace4ce97033e0f4b6d4b942b904
Christian Dupuis <cd@atomist.com>
2019-04-10 12:25:25 +0200

    Auto merge pull request #9 from atomist/sdm-pack-analysis
    * Add messaging
    
    * Autofix: tslint
    
    [atomist:generated] [atomist:autofix=tslint]
    
    * Autofix: TypeScript imports
    
    [atomist:generated] [atomist:autofix=typescript_imports]
    
    * Autofix: NPM package lock
    
    [atomist:generated] [atomist:autofix=npm_package_lock]
    
    * Add dismiss to push messages
    
    * Autofix: TypeScript header
    
    [atomist:generated] [atomist:autofix=typescript_header]
    
    * Autofix: TypeScript imports
    
    [atomist:generated] [atomist:autofix=typescript_imports]
    
    * Add message rewrite to dismiss
    
    * Address review comment

--- lib/analysis/Interpretation.ts
+++ lib/analysis/Interpretation.ts
@@ -31,10 +31,12 @@ import { DeliveryPhases } from "./phases";
 import { ProjectAnalysis } from "./ProjectAnalysis";
 import { ProjectAnalyzer } from "./ProjectAnalyzer";
 import { Scores } from "./Score";
+import { PushMessage } from "./support/messageGoal";
 
 /**
  * Consolidated interpretation. Unlike a ProjectAnalysis, an interpretation is not
  * intended to be persisted, but is only held in memory.
+ * Interpreters add to an Interpretation's goals, considering what goals may already have been set.
  */
 export interface Interpretation extends DeliveryPhases {
 
@@ -77,6 +79,12 @@ export interface Interpretation extends DeliveryPhases {
     readonly codeInspectionGoal: AutoCodeInspection;
 
     readonly scores: Scores;
+
+    /**
+     * Any messages regarding this project or push that should be displayed
+     * to users when handling the project.
+     */
+    readonly messages: PushMessage[];
 
 }
 
@@ -185,6 +193,16 @@ export function buildGoals(interpretation: Interpretation, analyzer: ProjectAnal
             g => (g as GoalWithPrecondition).dependsOn.push(...startup.goals, ...preCondition.goals));
     }
     return interpretation.buildGoals;
+}
+
+/**
+ * Messaging goals. Only set if there are messages in this interpretation.
+ */
+export function messagingGoals(interpretation: Interpretation, analyzer: ProjectAnalyzer): Goals {
+    if (interpretation.messages.length > 0) {
+        return goals("messaging").plan(analyzer.messageGoal);
+    }
+    return undefined;
 }
 
 export function testGoals(interpretation: Interpretation, analyzer: ProjectAnalyzer): Goals {
---
+++ lib/analysis/support/messageGoal.ts
@@ -0,0 +1,140 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    guid,
+    isSlackMessage,
+    MessageOptions,
+    SlackFileMessage,
+} from "@atomist/automation-client";
+import {
+    actionableButton,
+    CommandHandlerRegistration,
+    goal,
+    Goal,
+    GoalInvocation,
+    PreferenceScope,
+    SdmContext,
+    slackInfoMessage,
+    slackSuccessMessage,
+} from "@atomist/sdm";
+import {
+    Action,
+    italic,
+    SlackMessage,
+} from "@atomist/slack-messages";
+import * as crypto from "crypto";
+
+/**
+ * Message relating to this project
+ */
+export interface PushMessage {
+
+    readonly message: string | SlackMessage | SlackFileMessage & { title: string }; // require the title on file messages so that we can dismiss
+    readonly opts?: MessageOptions;
+}
+
+/**
+ * Factory that is able to produce PushMessages
+ */
+export type PushMessageFactory = (gi: GoalInvocation) => Promise<PushMessage[]>;
+
+/**
+ * Command to dismiss a certain PushMessage
+ */
+export const DismissMessageCommand: CommandHandlerRegistration<{ hash: string, msgId: string }> = {
+    name: "DismissMessage",
+    description: "Dismiss a Project Analysis message",
+    autoSubmit: true,
+    parameters: { hash: { displayable: false }, msgId: { displayable: false } },
+    listener: async ci => {
+        await ci.preferences.put(
+            `project-analysis.message.dismissed.${ci.parameters.hash}`,
+            true,
+            { scope: PreferenceScope.Sdm });
+        await ci.addressChannels(
+            slackSuccessMessage(
+                "Project Analysis",
+                "Successfully dismissed message"),
+            { id: ci.parameters.msgId });
+    },
+};
+
+/**
+ * Create a Goal that sends PushMessages.
+ * It adds a dismiss button to the messages it sends.
+ */
+export function messageGoal(messageFactory: PushMessageFactory): Goal {
+    return goal({ displayName: "Messages" }, async gi => {
+
+        const pushMessages = await messageFactory(gi) || [];
+
+        for (const pm of pushMessages) {
+            if (!(await isDismissed(pm, gi))) {
+                const options: MessageOptions = {
+                    id: guid(),
+                    ...(pm.opts || {}),
+                };
+
+                if (typeof pm.message === "string") {
+                    const msg = slackInfoMessage(
+                        "Project Analysis",
+                        pm.message,
+                        { actions: [createDismissAction(pm, options.id)] });
+                    await gi.addressChannels(msg, options);
+                } else if (isSlackMessage(pm.message)) {
+                    const msg = pm.message;
+                    if (!msg.attachments || msg.attachments.length === 0) {
+                        msg.attachments = [{
+                            fallback: "Dismiss",
+                            actions: [],
+                        }];
+                    }
+                    const attachment = msg.attachments.slice(-1)[0];
+                    attachment.actions = [...(attachment.actions || []), createDismissAction(pm, options.id)];
+                    await gi.addressChannels(msg, options);
+                } else {
+                    const fileMsg = pm.message as SlackFileMessage & { title: string };
+                    const msg = slackInfoMessage(
+                        "Project Analysis",
+                        `Dismiss ${italic(fileMsg.title)}`,
+                        { actions: [createDismissAction(pm, options.id)] });
+                    await gi.addressChannels(msg, options);
+                    await gi.addressChannels(pm.message);
+                }
+            }
+        }
+
+    });
+}
+
+function createDismissAction(pm: PushMessage, msgId: string): Action {
+    return actionableButton<{ hash: string, msgId: string }>(
+        { text: "Dismiss" },
+        DismissMessageCommand,
+        { hash: createHash(pm), msgId });
+}
+
+async function isDismissed(pm: PushMessage, context: SdmContext): Promise<boolean> {
+    return context.preferences.get<boolean>(
+        `project-analysis.message.dismissed.${createHash(pm)}`,
+        { defaultValue: false, scope: PreferenceScope.Sdm });
+}
+
+function createHash(pm: PushMessage): string {
+    const content = JSON.stringify(pm);
+    return crypto.createHash("md5").update(content).digest("base64").toString();
+}
