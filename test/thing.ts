fb8161201c97a8f5baf1c5ef47f8841d71780330
Rod Johnson <rod@atomist.com>
2019-06-10 16:58:52 +1000

    Add test script

---
+++ test/thing.ts
@@ -0,0 +1,6 @@
+import { DefaultProjectAnalyzerBuilder } from "../lib/analysis/support/DefaultProjectAnalyzerBuilder";
+
+console.log("b4")
+new DefaultProjectAnalyzerBuilder({} as any);
+
+//console.log(t)
